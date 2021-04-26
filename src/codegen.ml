c99e9445d029cc6bc874e84184731078145353a6
Claire-gip <chiara_boni@outlook.it>
2021-04-26 12:06:04 +0200

    fix codegen

--- src/codegen.ml
+++ src/codegen.ml
@@ -13,6 +13,9 @@ module L = Llvm
 (** The LLVM global context *)
 let llcontext = L.global_context()
 
+(** The LLVM global module *)
+let llmodule = L.create_module llcontext "microc"
+
 (** Some useful LLVM IR type to use in the code generation *)
 let int_type      = L.i32_type llcontext
 let float_type    = L.float_type llcontext
@@ -22,44 +25,40 @@ let void_type     = L.void_type llcontext
 let array_type    = L.array_type 
 let pointer_type  = L.pointer_type 
 
-(** Additional function that declares in the current module the print prototype 
-  @param llvm_module
-*)  
-let print_decl llvm_module =
+(** Additional function that declares in the current module the print prototype *)  
+let print_decl =
   let print_t = L.function_type void_type [| int_type |] in
-  L.declare_function "print" print_t llvm_module
+  L.declare_function "print" print_t llmodule
 
-(** Additional function that declares in the current module the getint prototype
-  @param llvm_module
-*)  
-let get_decl llvm_module =
+(** Additional function that declares in the current module the getint prototype *)  
+let get_decl =
   let getint_t = L.function_type int_type [| |] in
-  L.declare_function "getint" getint_t llvm_module
+  L.declare_function "getint" getint_t llmodule
 
 (** Additional function that converts Ast types into LLVM types
   @return lltype
 *)
 let rec convert_type (ty: typ) =
   match ty with
-  | TypI            -> int_type
-  | TypF            -> float_type
-  | TypB            -> bool_type
-  | TypC            -> char_type
-  | TypV            -> void_type
-  | TypA (t, i)     -> pointer_type (convert_type t)  
-  | TypP (t)        -> pointer_type (convert_type t)
-  | TypFun (lst, t) -> convert_type t
+  | TypI                -> int_type
+  | TypF                -> float_type
+  | TypB                -> bool_type
+  | TypC                -> char_type
+  | TypV                -> void_type
+  | TypA (t, i)         -> pointer_type (convert_type t)  
+  | TypP (t)            -> pointer_type (convert_type t)
+  | TypFun (t, id, lst) -> convert_type t
 
 (** 
   @return llvalue
 *)
-let rec initialize_var ty =
+let rec initialize_var (ty: typ) =
   match ty with
-  | TypI            -> L.const_int int_type 0
-  | TypF            -> L.const_float float_type 0.0
-  | TypB            -> L.const_int bool_type 0
-  | TypC            -> L.const_int char_type 0
-  | TypA (t, i)     -> (
+  | TypI                -> L.const_int int_type 0
+  | TypF                -> L.const_float float_type 0.0
+  | TypB                -> L.const_int bool_type 0
+  | TypC                -> L.const_int char_type 0
+  | TypA (t, i)         -> (
       let lltype = convert_type t in
       let llvalue = initialize_var t in
       let size =  
@@ -70,9 +69,42 @@ let rec initialize_var ty =
       let array_init = Array.make size llvalue in
       L.const_array lltype array_init
    )
-  | TypP (t)        -> L.const_pointer_null (convert_type t)
-  | _               -> Util.raise_semantic_error dummy_pos Error_msg.codegen_vardec_err
+  | TypP (t)            -> L.const_pointer_null (convert_type t)
+  | _                   -> failwith Error_msg.codegen_vardec_err
 
+(** A table mapping a binary operator in the LLVM 
+  function that implemets it and its name 
+*)
+let primitive_operators = 
+  [
+    (int_type, Add),      (L.build_add, "add") 
+  ; (float_type, Add),    (L.build_fadd, "add") 
+  ; (int_type, Mult),     (L.build_mul, "mul")
+  ; (float_type, Mult),   (L.build_fmul, "mul")
+  ; (int_type, Sub),      (L.build_sub, "sub")
+  ; (float_type, Sub),    (L.build_fsub, "sub")
+  ; (int_type, Div),      (L.build_sdiv, "div")
+  ; (float_type, Div),    (L.build_fdiv, "div")
+  ; (int_type, Mod),      (L.build_srem, "mod")
+  ; (float_type, Mod),    (L.build_frem, "mod")
+
+  ; (int_type, Equal),    (L.build_icmp L.Icmp.Eq, "equal") 
+  ; (float_type, Equal),  (L.build_fcmp L.Fcmp.Oeq, "equal") 
+  ; (int_type, Neq),      (L.build_icmp L.Icmp.Ne, "neq")
+  ; (float_type, Neq),    (L.build_fcmp L.Fcmp.One, "neq")
+  ; (int_type, Less),     (L.build_icmp L.Icmp.Slt, "less")
+  ; (float_type, Less),   (L.build_fcmp L.Fcmp.Olt, "less")
+  ; (int_type, Leq),      (L.build_icmp L.Icmp.Sle, "leq")
+  ; (float_type, Leq),    (L.build_fcmp L.Fcmp.Ole, "leq")
+  ; (int_type, Greater),  (L.build_icmp L.Icmp.Sgt, "greater")
+  ; (float_type, Greater),(L.build_fcmp L.Fcmp.Ogt, "greater")
+  ; (int_type, Geq),      (L.build_icmp L.Icmp.Sge, "geq")
+  ; (float_type, Geq),    (L.build_fcmp L.Fcmp.Oge, "geq")
+
+  ; (bool_type, And),     (L.build_and, "and")
+  ; (bool_type, Or),      (L.build_or, "or")
+  ]
+    
 (** 
   @param env the environment of llvalues
   @param params the list of parameters
@@ -97,15 +129,6 @@ let termination_stmt builder f =
   | None      -> ignore(f builder)
 
 (** 
-  @return llvalue
-*)
-let build_op fi ff lv1 lv2 label builder = 
-  let t = L.type_of lv1 in
-  match t with 
-  | int_type    -> fi lv1 lv2 label builder
-  | float_type  -> ff lv1 lv2 label builder
-
-(** 
   @param ex
   @param env the environment of llvalues
   @param builder
@@ -119,13 +142,18 @@ let rec codegen_expr ex env builder =
   let rec codegen_access acc env = 
     match acc.node with
     | AccVar (id)         -> (
-      let lvalue = 
         try Symbol_table.lookup id env
-        with NotFoundEntry -> Util.raise_semantic_error acc.loc Error_msg.name_err
-      in L.build_load lvalue id builder
+        with NotFoundEntry -> failwith Error_msg.name_err
     )
-    | AccDeref (e)        -> codegen_expr e env builder        
-    | AccIndex (a, e)     -> codegen_access a env
+    | AccDeref (e)        -> (
+        let lvalue = codegen_expr e env builder in
+        L.build_gep lvalue [| L.const_int int_type 0 |] "ptr" builder
+    )   
+    | AccIndex (a, e)     -> (
+        let lvalue = codegen_access a env in
+        let lexpr = codegen_expr e env builder in
+        L.build_gep lvalue [| L.const_int int_type 0; lexpr |] "idx" builder
+    )
 
   in
   match ex.node with
@@ -134,7 +162,7 @@ let rec codegen_expr ex env builder =
   | CLiteral (c)          -> L.const_int char_type (Char.code c)
   | BLiteral (b)          -> L.const_int bool_type (Bool.to_int b)
   | NLiteral ()           -> L.const_int void_type 0
-  | Access (acc)          -> codegen_access acc env
+  | Access (acc)          -> L.build_load (codegen_access acc env) "tmp" builder
   | Assign (acc, e)       -> (
       let lvalue = codegen_access acc env in
       let lexpr = codegen_expr e env builder in
@@ -146,45 +174,57 @@ let rec codegen_expr ex env builder =
       match u with
       | Neg   -> L.build_neg lexpr "neg" builder 
       | Not   -> L.build_not lexpr "not" builder
-      | Incr 
+      | Incr  -> (
+        let (llvm_operator, label) = List.assoc (L.type_of lexpr, Add) primitive_operators in 
+        llvm_operator lexpr (L.const_int int_type 1) label builder
+      )
       | Decr  -> (
-          let t = L.type_of lexpr in
-          match t with
-          | int_type   -> (
-              if (u == Incr) 
-              then L.build_add lexpr (L.const_int int_type 1) "incr" builder
-              else L.build_sub lexpr (L.const_int int_type 1) "decr" builder
-          )
-          | float_type -> (
-              if (u == Incr) 
-              then L.build_sub lexpr (L.const_float float_type 1.0) "decr" builder
-              else L.build_fsub lexpr (L.const_float float_type 1.0) "decr" builder
-          )
+        let (llvm_operator, label) = List.assoc (L.type_of lexpr, Sub) primitive_operators in 
+        llvm_operator lexpr (L.const_int int_type 1) label builder
       )
   )
   | BinaryOp (b, e1, e2)  -> (
       let le1 = codegen_expr e1 env builder in
       let le2 = codegen_expr e2 env builder in
-      match b with
-      | Add       -> build_op (L.build_add) (L.build_fadd) le1 le2 "add" builder
-      | Sub       -> build_op (L.build_sub) (L.build_fsub) le1 le2 "sub" builder
-      | Mult      -> build_op (L.build_mul) (L.build_fmul) le1 le2 "mul" builder
-      | Div       -> build_op (L.build_sdiv) (L.build_fdiv) le1 le2 "div" builder
-      | Mod       -> build_op (L.build_srem) (L.build_frem) le1 le2 "mod" builder
 
-      | Equal     -> build_op (L.build_icmp L.Icmp.Eq)  (L.build_fcmp L.Fcmp.Oeq) le1 le2 "equal" builder
-      | Neq       -> build_op (L.build_icmp L.Icmp.Ne)  (L.build_fcmp L.Fcmp.One) le1 le2 "neq" builder
-      | Less      -> build_op (L.build_icmp L.Icmp.Slt) (L.build_fcmp L.Fcmp.Olt) le1 le2 "less" builder
-      | Leq       -> build_op (L.build_icmp L.Icmp.Sle) (L.build_fcmp L.Fcmp.Ole) le1 le2 "leq" builder
-      | Greater   -> build_op (L.build_icmp L.Icmp.Sgt) (L.build_fcmp L.Fcmp.Ogt) le1 le2 "greater" builder
-      | Geq       -> build_op (L.build_icmp L.Icmp.Sge) (L.build_fcmp L.Fcmp.Oge) le1 le2 "geq" builder
-      
-      | And       -> L.build_and le1 le2 "and" builder
-      | Or        -> L.build_or  le1 le2 "or" builder
-
-      | _         -> Util.raise_semantic_error ex.loc Error_msg.unknown_op_err
+      let (llvm_operator, label) = List.assoc (L.type_of le1, b) primitive_operators in 
+      llvm_operator le1 le2 label builder
   )
-  | Call (id, lst)        -> L.const_int int_type 0
+  | Call (id, lst)        -> (
+      match id with
+      | "print"   -> (
+        let lvalue = L.lookup_function id llmodule in
+        match lvalue with
+        | None    -> failwith Error_msg.name_err
+        | Some(f) -> (
+          let le = codegen_expr (List.hd (lst)) env builder in
+          L.build_call f (Array.of_list ([] @ [le])) "" builder
+        )
+      )
+      | "getint"  -> (
+        let lvalue = L.lookup_function id llmodule in
+        match lvalue with
+        | None    -> failwith Error_msg.name_err
+        | Some(f) -> L.build_call f (Array.of_list ([])) id builder
+      )
+      | _         -> (
+        let rec gen_call lst env acc = 
+        match lst with
+        | []      -> acc
+        | x::xs   -> (
+            let lexpr = codegen_expr x env builder in
+            gen_call xs env (acc @ [lexpr])
+        )
+        in
+        let lvalue =
+          try Symbol_table.lookup id env 
+          with NotFoundEntry -> failwith Error_msg.name_err
+        in
+        let vlst = gen_call lst env [] in
+        match L.type_of lvalue with
+        | void_type   -> L.build_call lvalue (Array.of_list (vlst)) "" builder
+      )
+  )
 
 (** 
   @param fdec
@@ -196,9 +236,9 @@ let rec codegen_expr ex env builder =
 let rec codegen_stmt fdec env builder st =
   match st.node with
   | If (ex, s1, s2)         -> (
+      let bcont = L.append_block llcontext "cont" fdec in
       let bthen = L.append_block llcontext "then" fdec in
       let belse = L.append_block llcontext "else" fdec in
-      let bcont = L.append_block llcontext "cont" fdec in
   
       let bt = codegen_stmt fdec (Symbol_table.begin_block env) (L.builder_at_end llcontext bthen) s1 in
       let bf = codegen_stmt fdec (Symbol_table.begin_block env) (L.builder_at_end llcontext belse) s2 in 
@@ -211,16 +251,15 @@ let rec codegen_stmt fdec env builder st =
       L.builder_at_end llcontext bcont
   )
   | While (ex, s)           -> (
-      let bguard = L.append_block llcontext "while" fdec in
+      let bguard = L.append_block llcontext "guard" fdec in 
       let bbody  = L.append_block llcontext "wbody" fdec in
-      let bcont  = L.append_block llcontext "cont" fdec in
-
       L.build_br bguard builder |> ignore;
-
+      
       let bt = codegen_stmt fdec (Symbol_table.begin_block env) (L.builder_at_end llcontext bbody) s in
       termination_stmt bt (L.build_br bguard) |> ignore;
 
-      let code_ex = codegen_expr ex env builder in
+      let bcont  = L.append_block llcontext "cont" fdec in
+      let code_ex = codegen_expr ex env (L.builder_at_end llcontext bguard) in
       L.build_cond_br code_ex bbody bcont (L.builder_at_end llcontext bguard) |> ignore;
       
       L.builder_at_end llcontext bcont
@@ -245,12 +284,38 @@ let rec codegen_stmt fdec env builder st =
         | x::xs   -> (
           match x.node with
           | Dec (ty, id)    -> (
-            let lvalue = initialize_var ty in
-            let _ = L.set_value_name id lvalue in
-            let local = L.build_alloca (convert_type ty) id builder in
-            let env = Symbol_table.add_entry id local env in
-            L.build_store lvalue local builder |> ignore;
-            check_block xs env builder
+            match ty with
+            | TypA (t, i) -> (
+                let size =  
+                  match i with
+                  | Some(i) -> i
+                  | None    -> 1
+                in
+                let array_init = Array.make size (initialize_var t) in
+                let lvalue = L.const_array (convert_type t) array_init in
+                let local = L.build_alloca (array_type(convert_type t) size) id builder in
+                let env = Symbol_table.add_entry id local env in
+                let loc = L.build_gep local [| L.const_int int_type 0 |] "idx" builder in
+                L.build_store lvalue loc builder |> ignore;
+                
+                check_block xs env builder
+            )
+            | TypP (tp)   -> (
+                let lvalue = L.const_pointer_null (convert_type ty) in
+                let local = L.build_alloca (convert_type ty) id builder in
+                let env = Symbol_table.add_entry id local env in
+                L.build_store lvalue local builder |> ignore;
+
+                check_block xs env builder
+            )
+            | _           -> (
+                let lvalue = initialize_var ty in
+                let local = L.build_alloca (convert_type ty) id builder in
+                let env = Symbol_table.add_entry id local env in
+                L.build_store lvalue local builder |> ignore;
+
+                check_block xs env builder
+            )
           )
           | Stmt (s)        -> (
             let b = codegen_stmt fdec env builder s in
@@ -262,14 +327,15 @@ let rec codegen_stmt fdec env builder st =
   )
 
 (** 
-  @param llvm_module
   @param env the environment of llvalues
   @param topdecls
   @return llmodule
 *)
-let rec codegen_topdec llvm_module env topdecls =
+let rec codegen_topdec env topdecls =
   match topdecls with
-  | []      -> llvm_module
+  | []      -> ( Symbol_table.print_elems env |> ignore;
+      llmodule
+  )
   | x::xs   -> (
     match x.node with
     | Fundecl (fdec)    -> (
@@ -277,7 +343,7 @@ let rec codegen_topdec llvm_module env topdecls =
         let lst_par = List.map (fun (a,b) -> a) fdec.formals in
         let par_types = Array.of_list (List.map (convert_type) lst_par) in 
         let ftype = L.function_type (convert_type fdec.typ) par_types in
-        let dec = L.define_function fdec.fname ftype llvm_module in
+        let dec = L.define_function fdec.fname ftype llmodule in
         let env = Symbol_table.add_entry fdec.fname dec env in
   
         (* create bb *)
@@ -285,7 +351,7 @@ let rec codegen_topdec llvm_module env topdecls =
         let env' = 
           add_params (Symbol_table.begin_block env) fdec.formals (Array.to_list (L.params dec)) builder 
         in
-        let _ = codegen_stmt dec env' builder fdec.body in
+        let builder' = codegen_stmt dec env' builder fdec.body in
         
         (* return value *)
         let fret = 
@@ -293,13 +359,13 @@ let rec codegen_topdec llvm_module env topdecls =
             | TypV  -> L.build_ret_void
             | _     -> L.build_ret (L.const_int int_type 0) 
         in
-        termination_stmt builder fret |> ignore;
-        codegen_topdec llvm_module env xs
+        termination_stmt builder' fret |> ignore;
+        codegen_topdec env xs
     )
     | Vardec (ty, id)   -> (
-        let value =  L.define_global id (initialize_var ty) llvm_module in
-        Symbol_table.add_entry id value env |> ignore;
-        codegen_topdec llvm_module env xs
+        let value =  L.define_global id (initialize_var ty) llmodule in
+        let env = Symbol_table.add_entry id value env in
+        codegen_topdec env xs
     )
   )
 
@@ -307,9 +373,8 @@ let rec codegen_topdec llvm_module env topdecls =
   @return llmodule the entire generated module
 *)
 let to_ir (Prog(topdecls)) =
-  let llmodule = L.create_module llcontext "microc" in
-  print_decl llmodule |> ignore;
-  get_decl llmodule |> ignore;
+  print_decl |> ignore;
+  get_decl |> ignore;
 
   let env = Symbol_table.empty_table in
-    codegen_topdec llmodule env topdecls;
+    codegen_topdec env topdecls;
